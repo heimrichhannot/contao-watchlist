4ddaff5cead2443976cf9b64cd707565e578e8f5
Rico Kaltofen <r.kaltofen@heimrich-hannot.de>
2014-08-22 15:19:42 +0200

    added css class to tl_module pattern

--- dca/tl_module.php
+++ dca/tl_module.php
@@ -13,4 +13,4 @@ $dc = &$GLOBALS['TL_DCA']['tl_module'];
 /**
  * Palettes
  */
-$dc['palettes']['watchlist'] = '{title_legend},name,headline,type;{template_legend:hide},customTpl;{protected_legend:hide},protected;{expert_legend:hide},guests';
+$dc['palettes']['watchlist'] = '{title_legend},name,headline,type;{template_legend:hide},customTpl;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space';
