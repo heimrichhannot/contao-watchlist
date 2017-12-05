718601276b56017327cd019d713ac2af8c15d7bd
Konstantin Wagner <k.wagner@heimrich-hannot.de>
2017-12-05 15:44:02 +0100

    see cl

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,11 @@
 # Changelog
 All notable changes to this project will be documented in this file.
 
+## [2.0.9] - 2017-12-05
+
+### fixed
+- updated autoload 
+
 ## [2.0.8] - 2017-12-05
 
 ### changed
--- config/autoload.php
+++ config/autoload.php
@@ -3,7 +3,7 @@
 /**
  * Contao Open Source CMS
  *
- * Copyright (c) 2005-2015 Leo Feyer
+ * Copyright (c) 2005-2017 Leo Feyer
  *
  * @license LGPL-3.0+
  */
@@ -12,43 +12,52 @@
 /**
  * Register the namespaces
  */
-ClassLoader::addNamespaces([
-    'HeimrichHannot',
-]);
+ClassLoader::addNamespaces(array
+(
+	'HeimrichHannot',
+));
 
 
 /**
  * Register the classes
  */
-ClassLoader::addClasses([
-    // Models
-    'HeimrichHannot\Watchlist\WatchlistModel'              => 'system/modules/watchlist/models/WatchlistModel.php',
-    'HeimrichHannot\Watchlist\WatchlistItemModel'          => 'system/modules/watchlist/models/WatchlistItemModel.php',
+ClassLoader::addClasses(array
+(
+	// Modules
+	'HeimrichHannot\Watchlist\ModuleWatchlistDownloadList'    => 'system/modules/watchlist/modules/ModuleWatchlistDownloadList.php',
+	'HeimrichHannot\Watchlist\ModuleWatchlist'                => 'system/modules/watchlist/modules/ModuleWatchlist.php',
 
-    // Modules
-    'HeimrichHannot\Watchlist\ModuleWatchlist'             => 'system/modules/watchlist/modules/ModuleWatchlist.php',
-    'HeimrichHannot\Watchlist\ModuleWatchlistDownloadList' => 'system/modules/watchlist/modules/ModuleWatchlistDownloadList.php',
-]);
+	// Models
+	'HeimrichHannot\Watchlist\WatchlistModel'                 => 'system/modules/watchlist/models/WatchlistModel.php',
+	'HeimrichHannot\Watchlist\WatchlistItemModel'             => 'system/modules/watchlist/models/WatchlistItemModel.php',
+
+	// Classes
+	'HeimrichHannot\Watchlist\Controller\WatchlistController' => 'system/modules/watchlist/classes/controller/WatchlistController.php',
+	'HeimrichHannot\Watchlist\Controller\AjaxController'      => 'system/modules/watchlist/classes/controller/AjaxController.php',
+	'HeimrichHannot\Watchlist\Watchlist'                      => 'system/modules/watchlist/classes/Watchlist.php',
+));
 
 
 /**
  * Register the templates
  */
-TemplateLoader::addFiles([
-    'watchlist_edit_actions'        => 'system/modules/watchlist/templates/actions',
-    'watchlist_add_action'          => 'system/modules/watchlist/templates/actions',
-    'watchlist_global_actions'      => 'system/modules/watchlist/templates/actions',
-    'mod_watchlist'                 => 'system/modules/watchlist/templates/modules',
-    'watchlist_item'                => 'system/modules/watchlist/templates/items',
-    'watchlist_notify'              => 'system/modules/watchlist/templates/notifications',
-    'watchlist_grouped'             => 'system/modules/watchlist/templates/watchlist',
-    'watchlist'                     => 'system/modules/watchlist/templates/watchlist',
-    'watchlist_parents'             => 'system/modules/watchlist/templates/nav',
-    'watchlist_view_download'       => 'system/modules/watchlist/templates/views',
-    'watchlist_multiple_add_action' => 'system/modules/watchlist/templates/actions',
-    'watchlist_multiple'            => 'system/modules/watchlist/templates/watchlist',
-    'watchlist_select_actions'      => 'system/modules/watchlist/templates/actions',
-    'watchlist_downloadLink_action' => 'system/modules/watchlist/templates/actions',
-    'watchlist_download_list_item'  => 'system/modules/watchlist/templates/items',
-    'mod_watchlist_download_list'   => 'system/modules/watchlist/templates/modules',
-]);
+TemplateLoader::addFiles(array
+(
+	'mod_watchlist'                 => 'system/modules/watchlist/templates/modules',
+	'mod_watchlist_download_list'   => 'system/modules/watchlist/templates/modules',
+	'watchlist_notify'              => 'system/modules/watchlist/templates/notifications',
+	'watchlist_multiple'            => 'system/modules/watchlist/templates/watchlist',
+	'watchlist'                     => 'system/modules/watchlist/templates/watchlist',
+	'watchlist_grouped'             => 'system/modules/watchlist/templates/watchlist',
+	'watchlist_parents'             => 'system/modules/watchlist/templates/nav',
+	'watchlist_download_list_item'  => 'system/modules/watchlist/templates/items',
+	'watchlist_item'                => 'system/modules/watchlist/templates/items',
+	'watchlist_view_download'       => 'system/modules/watchlist/templates/views',
+	'watchlist_multiple_add_action' => 'system/modules/watchlist/templates/actions',
+	'watchlist_downloadLink_action' => 'system/modules/watchlist/templates/actions',
+	'watchlist_add_action'          => 'system/modules/watchlist/templates/actions',
+	'watchlist_edit_actions'        => 'system/modules/watchlist/templates/actions',
+	'watchlist_modal'               => 'system/modules/watchlist/templates/actions',
+	'watchlist_select_actions'      => 'system/modules/watchlist/templates/actions',
+	'watchlist_global_actions'      => 'system/modules/watchlist/templates/actions',
+));
